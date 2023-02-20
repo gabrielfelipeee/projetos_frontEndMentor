f0dabf068d6e405578ffb2f82fed783ee3385566
gabrielfelipeee <gabrielfeliperodrigues78@gmail.com>
2023-02-20 15:26:22 -0300

    Fixing menu bug

--- news-homepage-main/main.js
+++ news-homepage-main/main.js
@@ -10,24 +10,12 @@ menuClose.onclick = function() {
 };
 
 
-
-
 function screenWidth() {
-    let web3Mobile = document.querySelector('.img-web3-mobile');
-    let web3Desktop = document.querySelector('.img-web3-desktop');
-
-
     if (window.innerWidth >= 992) {
-        web3Mobile.style.display = 'none';
-        web3Desktop.style.display = 'block';
-
         iconMenu.style.display = 'none';
         menu.style.display = 'block';
         menuClose.style.display = 'none';
     } else {
-        web3Mobile.style.display = 'block';
-        web3Desktop.style.display = 'none';
-
         iconMenu.style.display = 'block';
         menu.style.display = 'none';
         menuClose.style.display = 'block';
